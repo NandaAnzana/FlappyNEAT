d8f68c2e03061f809d15bf1a9025a25fb0d42ef8
NandaAnzana <nanda.anzana@gmail.com>
2020-08-19 12:38:50 +0700

    Revise how the bird see

--- src/main_neat.py
+++ src/main_neat.py
@@ -25,7 +25,7 @@ def eval(genomes, config):
 	game_start = False
 	frame_count = 0
 	score = 0
-
+	pip_ind = 0
 	floor = Floor()
 	pipe_bottom = Pipe()
 	pipe_top = Pipe(True)
@@ -54,14 +54,7 @@ def eval(genomes, config):
 
 	    screen.blit(bg, (0,0))
 	    frame_count += 1
-	    if len(birds) > 0:
-	    	for ind_pipe in range(len(list_pipe)):
-		    	if birds[0].rect.midleft[0] < list_pipe[ind_pipe].midright[0]:
-		    		pip_ind = ind_pipe
-		    		break
-		    	else:
-		    		pass
-	    else:
+	    if len(birds) < 1:
 	    	train = False
 	    	break
 	    if frame_count%360 == 0:
@@ -71,8 +64,11 @@ def eval(genomes, config):
 	    	if i%2 == 0:
 	    		pipe_bottom.draw(screen, rect)
 	    		rect = pipe_bottom.update_rect
+	    		if (i == 0) and (rect.midright[0] == 100):
+	    			pip_ind = 2
 	    		if (i == 0) and (rect.center[0] <= -100):
 	    			list_pipe = list_pipe[2:]
+	    			pip_ind = 0
 	    		if (i == 0) and (rect.center[0] == 100):
 	    			for i, bird in enumerate(birds):
 	    				ge[i].fitness += 10
@@ -87,7 +83,7 @@ def eval(genomes, config):
 	    	ge[i].fitness += 0.1
 	    	output = nets[i].activate((bird.rect.centery, abs(bird.rect.centery - list_pipe[pip_ind].midtop[1]),
 										abs(bird.rect.centery - list_pipe[pip_ind+1].midbottom[1])))
-	    	if output[0] > 0.7:
+	    	if output[0] > 0.5:
 	    		bird.jump()
 	    birds, ge, nets = check_collision_neat(list_pipe, birds, ge, nets)
 	    count_score(screen, score)
